158ab5331d8b2a1b51210c9778890b251fdd5dca
José Robério <jose.silva2399@etec.sp.gov.br>
2025-12-05 19:48:42 -0300

    Create head.tsx

---
+++ APIHub-Nextjs/app/head.tsx
@@ -0,0 +1,15 @@
+export default function Head() {
+  return (
+    <>
+      <title>APIHub - Plataforma de APIs Gratuita</title>
+      <meta name="description" content="Descubra e integre APIs gratuitas em uma plataforma feita para desenvolvedores." />
+
+      {/* Favicon */}
+      <link rel="icon" href="/favicon.ico" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="manifest" href="/site.webmanifest" />
+    </>
+  )
+}
